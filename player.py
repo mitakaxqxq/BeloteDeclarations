c60ad413095191a551b5a9301453ca91697a6605
dimitar <mitko.box.hs@gmail.com>
2020-03-28 18:47:19 +0200

    Fixed player class and tests

--- player.py
+++ player.py
@@ -1,31 +1,31 @@
 class Player:
 
-	def __init__(self,name):
-		self.validate_name(name)
+    def __init__(self,name):
+        self.validate_name(name)
 
-		self.__name = name
-		self.__hand = []
-		self.__declarations = []
+        self.__name = name
+        self.__hand = []
+        self.__declarations = []
 
-	def get_name(self):
-		return self.__name
+    def get_name(self):
+        return self.__name
 
-	def set_declarations(self,declarations):
-		self.__declarations = declarations
+    def set_declarations(self,declarations):
+        self.__declarations = declarations
 
-	def set_hand(self,cards):
-		if len(cards) != 8:
-			raise ValueError('You must add exactly eight cards!')
-		else:
-			self.__cards = cards
+    def set_hand(self,cards):
+        if len(cards) != 8:
+            raise ValueError('You must add exactly eight cards!')
+        else:
+            self.__cards = cards
 
-	def get_hand(self):
-		return self.__cards
+    def get_hand(self):
+        return self.__cards
 
-	def __eq__(self,other):
-		return self.__name == other.__name
+    def __eq__(self,other):
+        return self.__name == other.__name
 
-	@staticmethod
-	def validate_name(name):
-		if not isinstance(name,str):
-			raise TypeError('Player name must be of string type!')
+    @staticmethod
+    def validate_name(name):
+        if not isinstance(name,str):
+            raise TypeError('Player name must be of string type!')
