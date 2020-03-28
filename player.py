e45e086c20be743723392720303a9119a04980b3
dimitar <mitko.box.hs@gmail.com>
2020-03-28 11:10:33 +0200

    Added player and team classes and tests

---
+++ player.py
@@ -0,0 +1,31 @@
+class Player:
+
+	def __init__(self,name):
+		self.validate_name(name)
+
+		self.__name = name
+		self.__hand = []
+		self.__declarations = []
+
+	def get_name(self):
+		return self.__name
+
+	def set_declarations(self,declarations):
+		self.__declarations = declarations
+
+	def set_hand(self,cards):
+		if len(cards) != 8:
+			raise ValueError('You must add exactly eight cards!')
+		else:
+			self.__cards = cards
+
+	def get_hand(self):
+		return self.__cards
+
+	def __eq__(self,other):
+		return self.__name == other.__name
+
+	@staticmethod
+	def validate_name(name):
+		if not isinstance(name,str):
+			raise TypeError('Player name must be of string type!')
---
+++ test_player.py
@@ -0,0 +1,68 @@
+import unittest
+
+from card import Card
+from player import Player
+
+class TestPlayer(unittest.TestCase):
+
+	def test_raise_exception_when_player_name_is_not_of_string_type(self):
+
+		exc = None
+
+		try:
+			player = Player(123)
+		except Exception as err:
+			exc = err
+
+		self.assertIsNotNone(exc)
+		self.assertEqual(str(exc),'Player name must be of string type!')
+
+	def test_raises_exception_when_a_player_has_different_number_of_eight_cards_in_hand(self):
+
+		list_of_cards = [Card('7', 'clubs'), Card('7', 'diamonds'), Card('7', 'hearts'), Card('7', 'spades'),
+						Card('8', 'clubs'), Card('8', 'diamonds'), Card('8', 'hearts')]
+
+		player = Player('Ivan')
+
+		exc = None
+
+		try:
+			player.set_hand(list_of_cards)
+		except Exception as err:
+			exc = err
+
+		self.assertIsNotNone(exc)
+		self.assertEqual(str(exc),'You must add exactly eight cards!')
+
+	def test_when_a_player_name_is_equal_to_another_player(self):
+
+		player1 = Player('Ivan')
+		player2 = Player('Ivan')
+
+		self.assertTrue(player1 == player2, 'Players are equal')
+
+	def test_if_get_name_method_returns_correctly_name_of_player(self):
+
+		player = Player('Ivan')
+
+		expected = 'Ivan'
+		result = player.get_name()
+
+		self.assertTrue(expected == result, 'Names are equal')
+
+
+	def test_if_get_hand_method_returns_correctly_hand_of_player(self):
+
+		list_of_cards = [Card('7', 'clubs'), Card('7', 'diamonds'), Card('7', 'hearts'), Card('7', 'spades'),
+						Card('8', 'clubs'), Card('8', 'diamonds'), Card('8', 'hearts'),Card('8', 'spades')]
+		
+		player = Player('Ivan')
+		player.set_hand(list_of_cards)
+
+		expected = [Card('7', 'clubs'), Card('7', 'diamonds'), Card('7', 'hearts'), Card('7', 'spades'),
+						Card('8', 'clubs'), Card('8', 'diamonds'), Card('8', 'hearts'),Card('8', 'spades')]
+
+		self.assertTrue(expected == player.get_hand(), 'Names are equal')
+
+if __name__ == '__main__':
+	unittest.main()
