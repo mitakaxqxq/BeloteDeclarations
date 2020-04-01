f9611a30dda4b0e73f865c3f28b75932fd4deed9
dimitar <mitko.box.hs@gmail.com>
2020-04-01 12:51:09 +0300

    Added new methods, fixed old methods

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import random
 from deck import Deck 
 from team import Team
+import json
+from magic_strings import *
 
 class Game:
 
@@ -19,6 +21,8 @@ class Game:
 
         self.__contract = ''
 
+        self.game_count = 0
+
     def get_order(self):
         return self.__order
 
@@ -30,7 +34,6 @@ class Game:
         self.__order.append(first_player)
 
     def select_contract(self):
-        announcements = ['clubs','diamonds','hearts','spades','no trumps','all trumps']
         index = random.randint(0,5)
         
         self.__contract = announcements[index]
@@ -40,38 +43,38 @@ class Game:
             player.set_hand(self.__deck.deal())
 
     def initialize_file_with_results(self):
-        with open("results.txt","w") as f:
+        with open("results.txt","a+") as f:
             name_of_first_team=self.__team1.get_name()
             f.write("       ")
             f.write(name_of_first_team)
-            f.write("       |       ")
+            f.write("               |               ")
             name_of_second_team=self.__team2.get_name()
             f.write("")
             f.write(name_of_second_team)
             f.write("     \n")
-            f.write("=================================")
+            f.write("============================================")
+            f.write("     \n")
 
-    def write_team_scores(self,score1,score2,current_round):
+    def write_team_scores(self,score1,score2):
 
         with open("results.txt","a+") as f:
-            if current_round == 1:
-                f.write("\n")
-                f.write(str(score1))
-                f.write("           |           ")
-                f.write(str(score2))
-                f.write("\n")
-            
-            else:
-                str1 = str(self.__team1_score) + ' + ' + str(score1)
+            str1 = str(self.__team1_score) + ' + ' + str(score1)
+            str2 = str(self.__team2_score) + ' + ' + str(score2)
+            f.write(str(str1))
+            f.write("               |               ")
+            f.write(str(str2))
+            f.write("\n")
 
-                str2 = str(self.__team2_score) + ' + ' + str(score2)
-
-                f.write(str(str1))
-                f.write("       |       ")
-                f.write(str(str2))
-                f.write("     \n")
-
-
+    def write_final_score(self,score1,score2):
+        with open("results.txt","a+") as f:
+            str1 = str(self.__team1_score)
+            str2 = str(self.__team2_score)
+            f.write(str(str1))
+            f.write("                   |                   ")
+            f.write(str(str2))
+            f.write("\n")
+            f.write("============================================")
+            f.write("\n")
 
     def check_for_belote(self):
         self.__team1.get_player1().get_belotes(self.__contract)
@@ -82,157 +85,171 @@ class Game:
         
         self.__team2.get_player2().get_belotes(self.__contract)
 
-    def calculate_score_of_teams(self,first=False,second=False):
-        if first == True:
+    def calculate_belotes_of_teams(self,team):
+        if team == 1:
             list_of_announcements = self.__team1.get_player1().get_announced_announcements()+self.__team1.get_player2().get_announced_announcements()
-
-            new_score = 0
-
-            for announcement in list_of_announcements:
-                if announcement == 'belote':
-                    self.__team1_score += 20
-                    new_score += 20
-
-                if announcement == 'tierce':
-                    self.__team1_score += 20
-                    new_score += 20
-
-                if announcement == 'quarte':
-                    self.__team1_score += 50
-                    new_score += 50
-
-                if announcement == 'quinte':
-                    self.__team1_score += 100
-                    new_score += 100
-
-            return new_score
-
-        elif second == True:
+            list_of_announcements = [elem for elem in list_of_announcements if elem == belote]
+            length = len(list_of_announcements)
+            return length*20
+        elif team == 2:
             list_of_announcements = self.__team2.get_player1().get_announced_announcements()+self.__team2.get_player2().get_announced_announcements()
+            list_of_announcements = [elem for elem in list_of_announcements if elem == belote]
+            length = len(list_of_announcements)
+            return length*20
 
+    def calculate_score_of_teams(self,team):
+        if team == 1:
+            list_of_announcements = self.__team1.get_player1().get_announced_announcements()+self.__team1.get_player2().get_announced_announcements()
             new_score = 0
-
+            list_of_announcements = [x for x in list_of_announcements if x != belote and x != carre]
             for announcement in list_of_announcements:
-                if announcement == 'belote':
-                    self.__team1_score += 20
-                    new_score += 20
-
-                if announcement == 'tierce':
-                    self.__team1_score += 20
-                    new_score += 20
-
-                if announcement == 'quarte':
-                    self.__team1_score += 50
-                    new_score += 50
-
-                if announcement == 'quinte':
-                    self.__team1_score += 100
-                    new_score += 100
+                new_score += announcements_values[announcement]
 
             return new_score
 
-    def check_for_carres_in_team_one(self):
-        first_list = self.__team1.get_player1().get_announcements_representation()+self.__team1.get_player2().get_announcements_representation()
+        elif team == 2:
+            list_of_announcements = self.__team2.get_player1().get_announced_announcements()+self.__team2.get_player2().get_announced_announcements()
+            new_score = 0
+            list_of_announcements = [x for x in list_of_announcements if x != belote and x != carre]
+            for announcement in list_of_announcements:
+                new_score += announcements_values[announcement]
 
-        carre_score = 0
-        for elem in first_list:
-            if len(elem) > 2:
-                if elem[0].get_value() == elem[1].get_value():
-                    
-                    if elem[0].get_value() == '10' or elem[0].get_value() == 'Q' or elem[0].get_value() == 'K' or elem[0].get_value() == 'A':
-                        self.__team1_score += 100
-                        carre_score = 100
-                    
-                    elif elem[0].get_value() == '9':
-                        self.__team1_score += 150
-                        carre_score = 150
-                    
-                    else:
-                        self.__team1_score += 200
-                        carre_score = 200
+            return new_score
 
-        return carre_score
-
-    def check_for_carres_in_team_two(self):
-        first_list = self.__team2.get_player1().get_announcements_representation()+self.__team2.get_player2().get_announcements_representation()
-
-        carre_score = 0
-        for elem in first_list:
-            if len(elem) > 2:
-                if elem[0].get_value() == elem[1].get_value():
-                    
-                    if elem[0].get_value() == '10' or elem[0].get_value() == 'Q' or elem[0].get_value() == 'K' or elem[0].get_value() == 'A':
-                        self.__team2_score += 100
-                        carre_score = 100
-                    
-                    elif elem[0].get_value() == '9':
-                        self.__team2_score += 150
-                        carre_score = 150
-                    
-                    else:
-                        self.__team2_score += 200
-                        carre_score = 200
-
-        return carre_score
-
+    def check_for_carres_in_teams(self,team):
+        if team == 1:
+            first_list = self.__team1.get_player1().get_announcements_representation()+self.__team1.get_player2().get_announcements_representation()
+            first_list = [elem for elem in first_list if len(elem) == 4]
+            carre_score = 0
+            for elem in first_list:
+                    if elem[0].get_value() == elem[1].get_value():
+                        if elem[0].get_value() == ten or elem[0].get_value() == queen or elem[0].get_value() == king or elem[0].get_value() == ace:
+                            carre_score = 100
+                        elif elem[0].get_value() == nine:
+                            carre_score = 150
+                        else:
+                            carre_score = 200
+            return carre_score
+        
+        elif team == 2:
+            first_list = self.__team2.get_player1().get_announcements_representation()+self.__team2.get_player2().get_announcements_representation()
+            first_list = [elem for elem in first_list if len(elem) == 4]
+            carre_score = 0
+            for elem in first_list:
+                    if elem[0].get_value() == elem[1].get_value():
+                        if elem[0].get_value() == ten or elem[0].get_value() == queen or elem[0].get_value() == king or elem[0].get_value() == ace:
+                            carre_score = 100
+                        elif elem[0].get_value() == nine:
+                            carre_score = 150
+                        else:
+                            carre_score = 200
+            return carre_score
 
     def compare_team_announcements(self):
         team1_highest_announcement = self.__team1.get_highest_announcement()
         team2_highest_announcement = self.__team2.get_highest_announcement()
+
         if team1_highest_announcement == [] and team2_highest_announcement == []:
             return
-
         if len(team1_highest_announcement) > len(team2_highest_announcement):
-            self.calculate_score_of_teams(first=True)
-
-
+            return 1
         elif len(team1_highest_announcement) < len(team2_highest_announcement):
-            self.calculate_score_of_teams(second=True)
-
+            return 2
         else:
             if team1_highest_announcement[-1].get_value() > team2_highest_announcement[-1].get_value():
-                self.calculate_score_of_teams(first=True)
-
+                return 1
             elif team1_highest_announcement[-1].get_value() < team2_highest_announcement[-1].get_value():
-                self.calculate_score_of_teams(second=True)
+                return 2
 
-
-    def play(self): #to do
+    def play(self):
 
         current_round = 1
 
+        self.__team1_score = 0
+        self.__team2_score = 0
+
+        self.game_count += 1
+
         while self.__team1_score <= 150 and self.__team2_score <= 150:
-
             self.__deck.shuffle_deck()
-
             self.deal_cards()
-
             self.select_contract()
-            
-            game_contract = self.__contract
-
             self.check_for_belote()
 
             round_points_team1 = 0
             round_points_team2 = 0
 
-            self.compare_team_announcements()
-            
-            round_points_team1 += self.check_for_carres_in_team_one()
-            round_points_team2 += self.check_for_carres_in_team_two()
+            if self.__contract == no_trumps:
+                self.__team1.get_player1().announced_announcements = []
+                self.__team1.get_player2().announced_announcements = []
+                self.__team2.get_player1().announced_announcements = []
+                self.__team2.get_player2().announced_announcements = []
+                self.write_team_scores(round_points_team1,round_points_team2)
+                result=self.round_dict_to_json(current_round,round_points_team1,round_points_team2)
+                self.write_to_json_file(result)
+                current_round += 1
+                self.rotate_players()
+                continue
 
-            self.write_team_scores(round_points_team1,round_points_team2,current_round)
+            result = self.compare_team_announcements()
+
+            if result == 1:
+                round_points_team1 += self.calculate_score_of_teams(result)
+            elif result == 2:
+                round_points_team2 += self.calculate_score_of_teams(result)
+
+            round_points_team1 += self.calculate_belotes_of_teams(1)
+            round_points_team2 += self.calculate_belotes_of_teams(2)
+
+            round_points_team1 += self.check_for_carres_in_teams(1)
+            round_points_team2 += self.check_for_carres_in_teams(2)
             
+            self.write_team_scores(round_points_team1,round_points_team2)
+            
+            self.__team1_score += round_points_team1
+            self.__team2_score += round_points_team2
+
+            result=self.round_dict_to_json(current_round,round_points_team1,round_points_team2)
+            self.write_to_json_file(result)
+            self.rotate_players()
             current_round += 1
 
-            self.rotate_players()
-
+        self.write_final_score(self.__team1_score,self.__team2_score)
         if self.__team1_score > self.__team2_score:
             return 1
         else:
             return 2
 
+    def round_dict_to_json(self,current_round,round_points_team1,round_points_team2):
+        round_dict = {f'Round {current_round}':
+                            {
+                                "Contract: ": self.__contract,
+                                str(self.__team1.get_name()):
+                                    {self.__team1.get_player1().get_name(): 
+                                        {"Cards": str(self.__team1.get_player1().get_hand()),
+                                        "announcement": str(self.__team1.get_player1().get_announced_announcements())}, 
+                                    self.__team1.get_player2().get_name(): 
+                                        {"Cards": str(self.__team1.get_player2().get_hand()),
+                                        "announcement": str(self.__team1.get_player2().get_announced_announcements())}, 
+                                    "Points": round_points_team1},
+                                str(self.__team2.get_name()):
+                                    {self.__team2.get_player1().get_name(): 
+                                        {"Cards": str(self.__team2.get_player1().get_hand()),
+                                    "announcement": str(self.__team2.get_player1().get_announced_announcements())}, 
+                                    self.__team2.get_player2().get_name(): 
+                                        {"Cards": str(self.__team2.get_player2().get_hand()),
+                                    "announcement": str(self.__team2.get_player2().get_announced_announcements())}, 
+                                    "Points": round_points_team2}
+                            }
+                    }
 
+        return round_dict
+
+    def write_to_json_file(self,round_dictionaries):
+        with open('data.json', 'a+') as f:
+            json_data = json.dumps({f"Game {self.game_count}" :round_dictionaries}, indent=4)
+            f.write(json_data)
+    
     @staticmethod
     def validate_values(team1,team2):
         if not isinstance(team1,Team):
